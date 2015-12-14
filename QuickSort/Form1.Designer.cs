5feb0394674bc0c0e24be82e8058caaaae56b6ad
patchamas <patchamas.k@gmail.com>
2015-12-14 17:43:02 +0700

    QuickSort

---
+++ QuickSort/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace QuickSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(55, 12);
+            this.tbInput.MaximumSize = new System.Drawing.Size(217, 20);
+            this.tbInput.MinimumSize = new System.Drawing.Size(217, 20);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(217, 20);
+            this.tbInput.TabIndex = 0;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 15);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(34, 13);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Input:";
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(197, 64);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.TabIndex = 2;
+            this.btnProcess.Text = "Run!";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(55, 38);
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.Size = new System.Drawing.Size(217, 20);
+            this.tbOutput.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Output:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 98);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.tbInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ QuickSort/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuickSort
+{
+    public partial class Form1 : Form
+    {
+        // IMPLEMENT DATE 08-12-2015 
+        // IMPLEMENT BY Patchamas Kakaew
+        // Form INTRODUCTION TO ALGORITHMS <Thomas H.Corman> 
+        /*
+         * QUICKSORT(A,p,r)
+         * if p < r
+         *  q = PARTITION(A,p,r)
+         *  QUICKSORT(A, p, q-1)
+         *  QUICKSORT(A, q+1, r)
+         * PARTITION(A, p, r)
+         * x=A[r]
+         * i=p-1
+         * for j=p to r-1
+         *  if A<=x
+         *  i=i+1
+         *  exchange A[i] with A[r]
+         * exchange A[i+1] with A[r]
+         * return i+1
+         */
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void QuickSort(int[] input, int low, int high)
+        {
+            int pivot_loc = 0;
+
+            if (low < high)
+            {
+                pivot_loc = partition(input, low, high);
+                QuickSort(input, low, pivot_loc - 1);
+                QuickSort(input, pivot_loc + 1, high);
+            }
+
+        }
+
+        public int partition(int[] input, int low, int high)
+        {
+            int pivot = input[high];
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                if (input[j] <= pivot)
+                {
+                    i++;
+                    swap(input, i, j);
+                }
+            }
+            swap(input, i + 1, high);
+            return i + 1;
+        
+        }
+
+        public void swap(int[] ar, int a, int b)
+        {
+            int temp = ar[a];
+            ar[a] = ar[b];
+            ar[b] = temp;
+        }
+
+        public void print(int[] output, TextBox tbOutput)
+        {
+            tbOutput.Clear();
+            for (int a = 0; a < output.Length; a++)
+            {
+                tbOutput.Text += output[a] + " ";
+            }
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {      
+            string[] arsInput = tbInput.Text.Split(' ');
+            int[] arInput = new int[arsInput.Length];
+            for (int i = 0; i < arsInput.Length; i++)
+            {
+                arInput[i] = Convert.ToInt16(arsInput[i]);              
+            }
+            QuickSort(arInput, 0, arsInput.Length-1);
+            print(arInput, tbOutput);
+        }
+
+    }
+}
